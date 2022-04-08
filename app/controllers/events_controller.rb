793413defeb3eba6db0d15ed1ffa33adb17de4b8
James Wu <ichangjwu@outlook.com>
2022-04-09 04:53:03 +0900

    Setup forms

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,31 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
     @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to controller: :users, action: :show, id: current_user.id
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date, :location)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events, foreign_key: 'creator_id'
+  validates :name, presence: true, uniqueness: true, length: { in: 4..10 }
 end
