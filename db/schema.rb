5d1e9536d2b8cca1e3703408ca259d3c8fc5cfae
James Wu <ichangjwu@outlook.com>
2022-04-09 02:24:42 +0900

    Add association and users#show view

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_08_161007) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_08_172211) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.date "date"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,8 +29,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_08_161007) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end
