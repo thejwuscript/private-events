674a6294c13c0d10eb00c86043250f361bc9910a
James Wu <ichangjwu@outlook.com>
2022-04-10 21:50:27 +0900

    Separate past/future events on Index page

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,15 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :event_attendings, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendings
+  scope :past, -> { where("date < ?", Time.now) }
+  scope :upcoming, -> { where("date > ?", Time.now) }
+
+  #def Event.past
+  #  Event.all.where("date < ?", Time.now)
+  #end
+
+  #def Event.upcoming
+  #  Event.all.where("date > ?", Time.now)
+  #end
+
 end
