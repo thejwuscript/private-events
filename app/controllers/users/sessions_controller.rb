68a81ac46c836814484435652df7faf2e410096f
James Wu <ichangjwu@outlook.com>
2022-04-11 21:57:11 +0900

    Fix Sign Out redirection

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,17 @@
+class Users::SessionsController < Devise::SessionsController
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :notice, :signed_out if signed_out
+    yield if block_given?
+    flash[:alert] = "You have signed out!"
+    respond_to_on_destroy
+  end
+
+  private
+
+  def respond_to_on_destroy
+    respond_to do |format|
+      format.all { redirect_to root_path, status: :see_other}
+    end
+  end
+end
