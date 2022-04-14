e721bcd91d234a6c9c3b8e7364a6caa03bfe87ee
James Wu <ichangjwu@outlook.com>
2022-04-14 21:13:05 +0900

    Add invitation functinality

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -9,6 +9,25 @@ class InvitationsController < ApplicationController
   end
   
   def create
+    @invitee = User.find_by(name: invitation_params[:invitee])
+    @event = Event.find_by(name: invitation_params[:event])
+    @invitation = Invitation.new
+    @invitation.invitee = @invitee
+    @invitation.event = @event
+
+    if @invitation.save
+      flash[:success] = "Invitation sent."
+      redirect_to root_path
+    else
+      flash.now[:error] = "Uh oh. Something went wrong."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def invitation_params
+    params.require(:invitation).permit(:invitee, :event)
   end
   
 end
